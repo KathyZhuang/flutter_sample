a350e8f7284254a4d3679349efab514f73901733
KathyZhuang <kathyzhuang378@gmail.com>
2022-02-26 15:04:48 -0500

    upgraded flutter

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Authentication {
   static Future<FirebaseApp> initializeFirebase() async {
